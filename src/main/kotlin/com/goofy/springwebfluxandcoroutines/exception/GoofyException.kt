d16d3083f5057e6ca1306b1e67be152362c87ba3
DongGeon0908 <wrjssmjdhappy@gmail.com>
2024-12-01 11:48:49 +0900

    fix: 불필요한 코드 제거

--- src/main/kotlin/com/goofy/springwebfluxandcoroutines/exception/GoofyException.kt
+++ src/main/kotlin/com/goofy/springwebfluxandcoroutines/exception/GoofyException.kt
@@ -7,20 +7,3 @@ open class GoofyException(
 ) : RuntimeException(message ?: errorCode.description)
 
 class NotFoundException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class InvalidTokenException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class InvalidRequestException(errorCode: ErrorCode, message: String? = null) : GoofyException(errorCode, message)
-
-class FailToCreateException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class AlreadyException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class NoAuthorityException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class FailToExecuteException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-class RedisPubSubException(errorCode: ErrorCode) : GoofyException(errorCode)
-
-/** Image Exception */
-class ImageUploadException : GoofyException(ErrorCode.IMAGE_CLIENT_UPLOAD_ERROR)
